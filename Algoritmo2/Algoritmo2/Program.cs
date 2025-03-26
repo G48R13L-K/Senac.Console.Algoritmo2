a4c317d8cfd8b045dbdc83ac05ddf6d7736693cd
GABRIELAUGUSTOKAPPENBERG <kappenberg4@gmail.com>
2025-03-25 21:50:34 -0300

    Leitura de variaveis.

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -1,6 +1,18 @@
-﻿int idade = 10;
+﻿int idade = 24;
 string nomeCliente = "Gabriel";
 decimal preco = 20M;
 char letra = 'a';
 var nomeCompleto = "Gabriel Augusto Kappenberg";
-var altura = 180;
+var altura = 165;
+Console.Write("Digite seu nome:");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Bem vindo " + nomeCompleto);
+Console.WriteLine();
+Console.Write("Digite sua idade: ");
+var linha = Console.ReadLine();
+idade = int.Parse(linha);    //Parse converte linhas em numenos
+Console.WriteLine("Idade informada: " + idade);
+if (idade < 18) 
+    Console.WriteLine("Usuário menor de idade, acesso negado.");
+Console.ReadKey();
